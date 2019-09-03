7ded9a5f7ca077d69b718b3e2947cb4a5281596f
Raja CSP Raman <raja.csp@gmail.com>
2019-09-02 22:23:15 -0400

    UseEffect base added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run');
+  });
+
   return (
     <div className="App">
-      <h1>Base Page</h1>
+      
+      
+      <div>
+        <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+      </div>
     </div>
   );
 }
