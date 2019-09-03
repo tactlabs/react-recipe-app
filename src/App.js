f76b610be1fb53c95737754732d7d859eedb808a
Raja CSP Raman <raja.csp@gmail.com>
2019-09-03 19:04:29 -0400

    Recipe undefined, empty map issue fixed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
+const App = () => {
 
-  const [counter, setCounter] = useState(0);
+  const APP_ID  = "cc614ec7";
+  const APP_KEY = "12c49017f3fd40021ab23937ec9946d5";
 
-  useEffect(() => {
-    console.log('Effect has been run');
+  //const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  //const [counter, setCounter] = useState(0);
+
+  const [recipes, setRecipes]  = useState([]);
+
+  useEffect( () => {
+    //console.log('Effect has been run');
+
+    getRecipes();
+
   }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+
+    const data = await response.json();
+
+    //console.log(data);
+
+    //console.log(data.hits);
+
+    //console.log(data.hits);
+
+    setRecipes(data.hits);
+  }
 
   return (
     <div className="App">
       
       
-      <div>
-        <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
-      </div>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+
+      {recipes.map( (recipe, index ) => (
+          <Recipe key={index} />
+      ))}
     </div>
   );
 }
---
+++ src/Recipe.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Recipe = () => {
+
+    return(
+        <div>
+            <h1>Title</h1>
+
+            <p>
+                Calories
+            </p>
+
+            <img src="" alt="" />
+        </div>
+    );
+}
+
+
+export default Recipe;
